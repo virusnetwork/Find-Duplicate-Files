bbe012701001c9f0d0be295510565219234a2932
Miles SIngleton <26247976+virusnetwork@users.noreply.github.com>
2019-07-24 09:02:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
+//  Find-Duplicate-files a program to find duplicate files
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 3
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 #include <iostream>
 #include <vector>
 #include <boost/filesystem.hpp>
 
+//function for finding duplicate files and inserting them into duplicateArray
 void fillFileArray(std::string folderLocation);
 
-std::vector<std::string> fileArray;
 std::vector<std::string> duplicateArray;
 
 namespace fs = boost::filesystem;
@@ -20,6 +36,7 @@ int main(int argc, char* argv[]) {
 
     fillFileArray(argv[1]);
 
+    //prints out all duplicate files and uses boosts remove function, should work on any system
     for (auto &element : duplicateArray)
     {
         std::cout << "\t" << element <<  std::endl;
@@ -33,6 +50,7 @@ int main(int argc, char* argv[]) {
 void fillFileArray(std::string folderLocation) {
     fs::path folderPath(folderLocation);
     fs::recursive_directory_iterator end_itr;
+    std::vector<std::string> fileArray;
 
     // cycle through the directory
     for (fs::recursive_directory_iterator itr(folderPath); itr != end_itr; ++itr) {
@@ -55,5 +73,7 @@ void fillFileArray(std::string folderLocation) {
             }
         }
     }
+    //empty vector to save memory as file array no longer needed
+    std::vector<std::string>().swap(fileArray);
 }
 
